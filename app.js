bada717b66a07c719a085cf7133426ed2afeb75d
naye1443 <eyaneubanks@gmail.com>
2022-10-30 23:39:57 -0400

    Add basic server functionality:     - Add program entry point.     - Add index pug file(temporary).     - Add routes for future.

---
+++ app.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// storing relative file paths to routes
+const indexRouter = require('./routes/index');
+
+const app = express();  // creates an expess object
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname + 'public')));
+
+// sets up routes to different views
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
