fdade644051d1189412ccdd1bba1b79ddd39130b
naye1443 <eyaneubanks@gmail.com>
2022-11-13 22:37:15 -0500

    Added a database that can be used.
    Needs to be initalized with test data everytime emulator is started

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('../../Auth_key.json');  // key to use database
 const router = express.Router();
+
+// initalizes database
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://happy-8293-default-rtdb.firebaseio.com/"
+});
 
 router.get('/',(req, res, next) =>
 {
     res.render('index', {title: 'Hey', message: 'Hello There!'});
 });
+
+router.post('/', async(req, res, next) =>
+{
+    const user = req.body;
+
+    await admin.firestore().collection('users').add(user);
+
+    res.status(201).send();
+}
+)
 
 module.exports = router;
